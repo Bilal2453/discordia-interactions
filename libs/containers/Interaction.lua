b1fac4cae2f2526b57c33c62679bd17a18b98a12
Bilal2453 <belal2453@gmail.com>
2021-11-07 00:02:47 +0300

    doc comments

--- libs/containers/Interaction.lua
+++ libs/containers/Interaction.lua
@@ -1,3 +1,11 @@
+--[=[
+@c Interaction x Snowflake
+@p data table
+@p parent Client
+@d Represents a [Discord Interaction](https://discord.com/developers/docs/interactions/receiving-and-responding#interactions)
+allowing you to respond and reply to user interactions.
+]=]
+
 local discordia = require("discordia")
 local Resolver = require("client/Resolver")
 local enums = require("enums")
@@ -101,6 +109,18 @@ function Interaction:_sendFollowup(msg)
   return data and self._channel._messages:_insert(data), err
 end
 
+--[=[
+@m reply
+@t http
+@p data string/table
+@op isEphemeral boolean
+@r ??
+@d Constructs a new reply to said interaction. If this is the first reply an initial one is sent,
+if previous reply was a deferred response, this will edit it to provided content,
+otherwise if previous reply is fully sent, this sends a followup response.
+
+Returns may not be constant because blame Discord.
+]=]
 function Interaction:reply(msg, isEphemeral)
   local msgType = type(msg)
   if msgType == "table" then
@@ -127,17 +147,48 @@ function Interaction:reply(msg, isEphemeral)
   return method(self, msg) -- TODO: make sure returns are constant, or should they not be constant?
 end
 
+--[=[
+@m replyDeferred
+@t http
+@op isEphemeral boolean
+@r Message
+@d Constructs an initial response that's deferred.
+Deferred reply can only be an initial response, and when used it will basically send a message that says
+"Bot is thinking...", and once `:reply` is used again it will edit that message.
+
+Returns the newly sent message (?).
+]=]
 function Interaction:replyDeferred(isEphemeral)
   local msg = isEphemeral and {flags = messageFlag.ephemeral} or nil
   return self:_sendMessage(msg, true)
 end
 
+--[=[
+@m getReply
+@t http
+@op id Message-ID-Resolvable
+@r Message
+@d Returns a reply message with the provided id, if no id was provided the original response message is returned.
+
+**Ephemeral Messages Cannot be Retrieved**
+]=]
 function Interaction:getReply(id)
   id = Resolver.messageId(id) or "@original"
   local data, err = self._api:getWebhookMessage(self._application_id, self._token, id)
   return data and self._channel._messages:_insert(data), err
 end
 
+--[=[
+@m editReply
+@t http
+@p payload table/string
+@op id Message-ID-Resolvable
+@r Message
+@d Modifies a response message with the provided id, if no id was provided original response is modified instead.
+`payload` is raw Message-alike table, if string is provided it is treated as `content` field
+
+**Ephemeral Messages Cannot be Modified**
+]=]
 function Interaction:editReply(payload, id)
   id = Resolver.messageId(id) or self._message.id
   payload = type(payload) == "table" and payload or {content = tostring(payload)}
@@ -145,6 +196,16 @@ function Interaction:editReply(payload, id)
   return data and true, err
 end
 
+--[=[
+@m deleteReply
+@t http
+@p payload table/string
+@r Message
+@d Modifies a response message with the provided id, if no id was provided original response is modified instead.
+`payload` is raw Message-alike table, if string is provided it is treated as `content` field
+
+**Ephemeral Messages Cannot be Modified**
+]=]
 function Interaction:deleteReply(id)
   id = Resolver.messageId(id) or "@original"
   local data, err = self._api:deleteWebhookMessage(self._application_id, self._token, id)
@@ -165,6 +226,15 @@ function Interaction:_sendUpdate(payload)
   end
 end
 
+--[=[
+@m update
+@t http
+@p data table/string
+@r Message
+@d Responds to a component-based interaction by modifying the original bot's message. `msg` is similar to editReply.
+
+Returns the original modified bot message (?).
+]=]
 function Interaction:update(msg)
   assert(self._message, "UPDATE_MESSAGE is only supported by components-based interactions!")
   if msg == nil and not self._initialRes then
@@ -179,54 +249,77 @@ function Interaction:update(msg)
   end
 end
 
+--[=[
+@m updateDeferred
+@t http
+@r Message
+@d Responds to a component-based interaction by acknowledging the interaction,
+where the next used `reply`/`update` methods will update the original bot message.
+
+Returns the original bot message (?).
+]=]
 function Interaction:updateDeferred()
   return self:_sendUpdate()
 end
 
+--[=[@p applicationId string A unique snowflake ID of the application.]=]
 function get.applicationId(self)
   return self._application_id
 end
 
+--[=[@p type number The Interaction type, see interactionType enum.]=]
 function get.type(self)
   return self._type
 end
 
+--[=[@p guildId string/nil A snowflake ID of the guild the interaction was made in if any.]=]
 function get.guildId(self)
   return self._guild_id
 end
 
+--[=[@p guild Guild/nil The guild object of self.guildId if any.]=]
 function get.guild(self)
   return self._guild
 end
 
+--[=[@p channelId string A snowflake ID of the channel the interaction was made in.
+Should always be provided, but keep in mind Discord flag it as optional for future-proofing.]=]
 function get.channelId(self)
   return self._channel_id
 end
 
+--[=[@p channel channel/nil The channel object of self.channelId.]=]
 function get.channel(self)
   return self._channel
 end
 
+--[=[@p message message/nil The message the interaction was used on. Currently only for components-based interactions.]=]
 function get.message(self)
   return self._message
 end
 
+--[=[@p member member/nil The member who made the interaction if it was in guild.]=]
 function get.member(self)
   return self._member
 end
 
+--[=[@p user member The user who made the interaction, should exists always.]=]
 function get.user(self)
   return self._user
 end
 
+--[=[@p data table The raw data of the interaction. See [Interaction Data Structure](https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-object-resolved-data-structure)]=]
 function get.data(self)
   return self._data
 end
 
+--[=[@p token string The interaction token. What allows you to responds to a specific interaction.
+This is a secret and shouldn't be exposed, if leaked anyone can send messages on behalf of your bot]=]
 function get.token(self)
   return self._token
 end
 
+--[=[@p version string The interaction version. (aka not useful at all)]=]
 function get.version(self)
   return self._version
 end
